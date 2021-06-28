8666dd1352c84240fa19480643661234b75e66fe
newoni <newoni_@naver.com>
2021-06-28 00:12:27 -0700

    make spring project <21.06.28> KH

---
+++ src/main/java/com/example/demo/configuraion/WebMvcConfig.java
@@ -0,0 +1,8 @@
+package com.example.demo.configuraion;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+}
---
+++ src/main/java/com/example/demo/controller/controller.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class controller {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String helloworld() {
+        return "helloworld";         // 실제 호출될 /WEB-INF/jsp/views/viewtest.jsp
+    }
+}
